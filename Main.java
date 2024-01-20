30bd77cd724f224fdfd6a0dc2b623cd44847a82d
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-01-20 21:16:59 +0000

    update

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello, Fucker");
+        String firstName = "James", lastName = "Owusu",  fullName = firstName + " " + lastName;
+        
+        System.out.println(fullName);
     }
 }
