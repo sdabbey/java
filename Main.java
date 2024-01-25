09d80e45d7c9c41699871016f949d050ccb57ba0
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-01-25 11:03:19 +0000

    added code for checking prime number

--- Main.java
+++ Main.java
@@ -9,10 +9,7 @@ public class Main{
         System.out.println(myInt);   
         */
 
-        
-
-        //Using ternary operator as and if...else shorthand
-        
+        //Using ternary operator as and if...else shorthand    
         /* 
         boolean isJavaFun = true;
         boolean isFishTasty = false;
@@ -23,6 +20,37 @@ public class Main{
         System.out.println(fishResult);
         */
 
+
+        //Switch 
+        /* 
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        
+            default:
+                break;
+        }
+        */
 
     }
 }
