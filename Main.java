c2b590eacbcc5e0072391a7dd4cbbd788275c74f
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-01-25 09:13:52 +0000

    update

--- Main.java
+++ Main.java
@@ -1,21 +1,27 @@
 public class Main{
     public static void main(String[] args){
-        //String firstName = "James", lastName = "Owusu",  fullName = firstName + " " + lastName;
-        //float f1 = 35e3f;
-        // double d1 = 12E4d;
-        // System.out.println(d1);
+     
+        /*
         double myDouble = 9.782345456d;
         float myInt = (float) myDouble; // Manual casting: double to int
 
+        System.out.println(myDouble);   
+        System.out.println(myInt);   
+        */
+
+        
+
+        //Using ternary operator as and if...else shorthand
+        
+        /* 
         boolean isJavaFun = true;
         boolean isFishTasty = false;
-
+        
         String javaResult = (isFishTasty) ? "Fish is tasty" : "Fish is not tasty";
         String fishResult = (isJavaFun) ? "Java is fun" : "Java is not fun";
         System.out.println(javaResult);
         System.out.println(fishResult);
-        System.out.println(myDouble);   // Outputs 9.78
-        System.out.println(myInt);      // Outputs 9
+        */
 
 
     }
