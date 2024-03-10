3474e852c1e6e13188e542de87a21d47ef6e4e26
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-03-10 23:52:03 +0000

    update

--- newday/MidsemAssignment.java
+++ newday/MidsemAssignment.java
@@ -3,17 +3,7 @@ import java.util.Scanner;
 public class MidsemAssignment {
     public static void main(String[] args) {
         //Instantiate Scanner class
-        Scanner input = new Scanner(System.in);
         
-        System.out.println("Enter the number of students: ");
-        int class_size = input.nextInt();
-
-        //Declare and Initialize variables
-        String name [] = new String[class_size];
-        String phone_number [] = new String[class_size];
-        int hdrating [] = new int[class_size];
-
-        System.out.println("Fill in individual student detail below: ");
         
         for (int i = 0; i < class_size; i++) {
             try {
